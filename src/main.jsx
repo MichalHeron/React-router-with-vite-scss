a377e8a001bb268c924d8af4f35cd9498e7fb5db
michal <michklak91@gmail.com>
2023-04-14 09:48:50 +0200

    useLoaderData added

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // import "../style/index.scss";
-import '../style/index.scss'
-import Root from '../routes/root'
-import ErrorPage from '../routes/error-page'
-import Contact from '../routes/contact'
+import './style/index.scss'
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from './routes/error-page'
+import Contact from './routes/contact'
+
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Root />,
 		errorElement: <ErrorPage />,
+		loader: rootLoader,
 		children: [
 			{
 				path: 'contacts/:contactId',
@@ -19,6 +21,10 @@ const router = createBrowserRouter([
 			},
 		],
 	},
+	// {
+	// 	path: "contacts/:contactId", // twozenie nowego routingu 
+	// 	element: <Contact />,
+	//   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,13 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLoaderData } from 'react-router-dom'
+import { getContacts } from '../contacts'
+
+export async function loader() {
+	const contacts = await getContacts()
+	return { contacts }
+}
 
 export default function Root() {
+	const { contacts } = useLoaderData()
 	return (
 		<>
 			<div id='sidebar'>
@@ -16,16 +23,28 @@ export default function Root() {
 					</form>
 				</div>
 				<nav>
-					<ul>
-						<li>
-							{/* <a href={`/contacts/1`}>Your Name</a>  nie ma ponownego renderowania calej storny dzieki zamianie na linkto*/}
-							<Link to={'/contacts/1'}>Your Name</Link>
-						</li>
-						<li>
-							{/* <a href={`/contacts/2`}>Your Friend</a> */}
-							<Link to={'/contacts/2'}>Your Friend</Link>
-						</li>
-					</ul>
+					{contacts.length ? (
+						<ul>
+							{contacts.map(contact => (
+								<li key={contact.id}>
+									<Link to={`contacts/${contact.id}`}>
+										{contact.first || contact.last ? (
+											<>
+												{contact.first} {contact.last}
+											</>
+										) : (
+											<i>No Name</i>
+										)}{' '}
+										{contact.favorite && <span>★</span>}
+									</Link>
+								</li>
+							))}
+						</ul>
+					) : (
+						<p>
+							<i>No contacts</i>
+						</p>
+					)}
 				</nav>
 			</div>
 			<div id='detail'>
@@ -34,3 +53,16 @@ export default function Root() {
 		</>
 	)
 }
+
+// {/* <nav>
+// 					<ul>
+// 						<li>
+// 							{/* <a href={`/contacts/1`}>Your Name</a>  nie ma ponownego renderowania calej storny dzieki zamianie na linkto */}
+// 							<Link to={'/contacts/1'}>Your Name</Link>
+// 						</li>
+// 						<li>
+// 							{/* <a href={`/contacts/2`}>Your Friend</a> */}
+// 							<Link to={'/contacts/2'}>Your Friend</Link>
+// 						</li>
+// 					</ul>
+// 				</nav> */}
