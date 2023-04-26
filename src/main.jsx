aad0c27b6c147a6f177055511847f8bd454668d5
michal <michklak91@gmail.com>
2023-04-26 13:56:24 +0200

    delete contact function added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import ErrorPage from './routes/error-page'
 import Contact, { loader as contactLoader } from './routes/contact'
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import EditContact, { action as editAction } from './routes/edit'
+import { action as destroyContact } from './routes/destroy'
 
 const router = createBrowserRouter([
 	{
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
 				element: <EditContact />,
 				loader: contactLoader, //There is no reason to attempt to share loaders among routes, they usually have their own
 				action: editAction,
+			},
+			{
+				path: 'contacts/:contactId/destroy',
+				action: destroyContact, //destroyAction
 			},
 		],
 	},
