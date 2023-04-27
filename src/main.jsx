b3e096338c206016775b4dbffd3bc349db3e0e5e
michal <michklak91@gmail.com>
2023-04-27 21:29:22 +0200

    pathless routings (last) and jsx routes option

--- src/main.jsx
+++ src/main.jsx
@@ -54,3 +54,45 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<RouterProvider router={router} />
 	</React.StrictMode>
 )
+
+
+
+// JSX Routes
+// And for our final trick, many folks prefer to configure their routes with JSX. You can do that with createRoutesFromElements. There is no functional difference between JSX or objects when configuring your routes, it's simply a stylistic preference.
+
+// import {
+// 	createRoutesFromElements,
+// 	createBrowserRouter,
+//   } from "react-router-dom";
+  
+//   const router = createBrowserRouter(
+// 	createRoutesFromElements(
+// 	  <Route
+// 		path="/"
+// 		element={<Root />}
+// 		loader={rootLoader}
+// 		action={rootAction}
+// 		errorElement={<ErrorPage />}
+// 	  >
+// 		<Route errorElement={<ErrorPage />}>
+// 		  <Route index element={<Index />} />
+// 		  <Route
+// 			path="contacts/:contactId"
+// 			element={<Contact />}
+// 			loader={contactLoader}
+// 			action={contactAction}
+// 		  />
+// 		  <Route
+// 			path="contacts/:contactId/edit"
+// 			element={<EditContact />}
+// 			loader={contactLoader}
+// 			action={editAction}
+// 		  />
+// 		  <Route
+// 			path="contacts/:contactId/destroy"
+// 			action={destroyAction}
+// 		  />
+// 		</Route>
+// 	  </Route>
+// 	)
+//   );
